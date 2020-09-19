585161a3cda2d10432320dca593bf97c0036df22
jians10 <shenxin.jiang@vanderbilt.edu>
2020-09-20 06:09:22 +0800

    Try This
    I have not yet implemented the crosshair, so please aim with the center of the screen to emit grabbling gun

--- ParkourDemo/Assets/Scripts/PlayerScript/PlayerController.cs
+++ ParkourDemo/Assets/Scripts/PlayerScript/PlayerController.cs
@@ -10,9 +10,14 @@ public class PlayerController : MonoBehaviour
    
     float verticalLookRotation;
     bool grounded;
+    Vector3 crouchScale = new Vector3(1, 0.5f, 1);
     Vector3 smoothMoveVelocity;
     Vector3 moveAmount;
-
+    Vector3 playerScale = new Vector3(1, 1, 1);
+    float slideForce = 400;
+    public float slideCounterMovement = 0.2f;
+    public float moveSpeed = 2000;
+    [SerializeField] bool isWallRight, isWallLeft;
     PhotonView PV;
 
     Rigidbody rb;
@@ -42,12 +47,13 @@ public class PlayerController : MonoBehaviour
 
         if (!PV.IsMine)
             return;
-        
+        CheckForWall();
         Look();
         Move();
         Jump();
-
-
+        Counch();
+        WallRunInput();
+        
     }
 
     void Look()
@@ -66,6 +72,45 @@ public class PlayerController : MonoBehaviour
 
         moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
     }
+
+
+    void Counch() {
+
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCrouch();
+        }
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            StopCrouch();
+        }
+    }
+
+
+    private void StartCrouch()
+    {
+        Debug.Log("Crouching");
+        transform.localScale = crouchScale;
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+        if (rb.velocity.magnitude > 0.5f)
+        {
+            if (grounded)
+            {
+                rb.AddForce(transform.forward * slideForce);
+                rb.AddForce(moveSpeed * Time.deltaTime * -rb.velocity.normalized * slideCounterMovement);
+            }
+        }
+    }
+
+    private void StopCrouch()
+    {
+        transform.localScale = playerScale;
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+    }
+
+
+
 
 
     public void SetGroundedState(bool _grounded)
@@ -92,4 +137,58 @@ public class PlayerController : MonoBehaviour
 
         rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
     }
+
+
+    //Wallrunning
+    public LayerMask whatIsWall;
+    public float wallrunForce, maxWallrunTime, maxWallSpeed;
+    
+    bool isWallRunning;
+    public float maxWallRunCameraTilt, wallRunCameraTilt;
+    public Transform orientation;
+    private void WallRunInput() //make sure to call in void Update
+    {
+        //Wallrun
+        if (Input.GetKey(KeyCode.D) && isWallRight) StartWallrun();
+        if (Input.GetKey(KeyCode.A) && isWallLeft) StartWallrun();
+    }
+    private void StartWallrun()
+    {
+        Debug.Log("StartWallRunn");
+        rb.useGravity = false;
+        isWallRunning = true;
+        //allowDashForceCounter = false;
+
+        if (rb.velocity.magnitude <= maxWallSpeed)
+        {
+            rb.AddForce(orientation.forward * wallrunForce * Time.deltaTime);
+
+            //Make sure char sticks to wall
+            if (isWallRight)
+                rb.AddForce(orientation.right * wallrunForce / 5 * Time.deltaTime);
+            else
+                rb.AddForce(-orientation.right * wallrunForce / 5 * Time.deltaTime);
+        }
+    }
+    private void StopWallRun()
+    {
+        isWallRunning = false;
+        rb.useGravity = true;
+    }
+    private void CheckForWall() //make sure to call in void Update
+    {
+        isWallRight = Physics.Raycast(transform.position, orientation.right, 2f, whatIsWall);
+        isWallLeft = Physics.Raycast(transform.position, -orientation.right, 2f, whatIsWall);
+
+        //leave wall run
+        if (!isWallLeft && !isWallRight) StopWallRun();
+        //reset double jump (if you have one :D)
+        //if (isWallLeft || isWallRight) doubleJumpsLeft = startDoubleJumps;
+    }
+
+
+    /// <summary>
+    /// Wall run done, here comes the rest of the movement script
+    /// </summary>
+
 }
